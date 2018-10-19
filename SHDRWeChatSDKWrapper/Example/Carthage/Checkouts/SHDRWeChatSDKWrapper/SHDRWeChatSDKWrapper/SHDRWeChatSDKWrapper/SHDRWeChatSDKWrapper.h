93c5e4afa7f8095faab1b6b2e0448cf60672bef1
Kong, Jay X. -ND <Jay.X.Kong.-ND@disney.com>
2018-10-19 14:11:19 +0800

    after carthage build

---
+++ SHDRWeChatSDKWrapper/Example/Carthage/Checkouts/SHDRWeChatSDKWrapper/SHDRWeChatSDKWrapper/SHDRWeChatSDKWrapper/SHDRWeChatSDKWrapper.h
@@ -0,0 +1,21 @@
+//
+//  SHDRWeChatSDKWrapper.h
+//  SHDRWeChatSDKWrapper
+//
+//  Created by Kong, Jay X. -ND on 2018/10/19.
+//  Copyright © 2018 Kong, Jay X. -ND. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "WXApi.h"
+#import "WXApiObject.h"
+#import "WechatAuthSDK.h"
+//! Project version number for SHDRWeChatSDKWrapper.
+FOUNDATION_EXPORT double SHDRWeChatSDKWrapperVersionNumber;
+
+//! Project version string for SHDRWeChatSDKWrapper.
+FOUNDATION_EXPORT const unsigned char SHDRWeChatSDKWrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SHDRWeChatSDKWrapper/PublicHeader.h>
+
+
