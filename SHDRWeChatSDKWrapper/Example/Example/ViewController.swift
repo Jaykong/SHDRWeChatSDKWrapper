6e997817d66d006653d05341d1bca18c2eea65f1
Kong, Jay X. -ND <Jay.X.Kong.-ND@disney.com>
2018-10-19 13:24:46 +0800

    First init

---
+++ SHDRWeChatSDKWrapper/Example/Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Kong, Jay X. -ND on 2018/10/19.
+//  Copyright © 2018 Kong, Jay X. -ND. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ SHDRWeChatSDKWrapper/SHDRWeChatSDKWrapper/WeChatSDK/WeChatSDK.swift
@@ -0,0 +1,9 @@
+//
+//  WeChatSDK.swift
+//  SHDRWeChatSDKWrapper
+//
+//  Created by Kong, Jay X. -ND on 2018/10/19.
+//  Copyright © 2018 Kong, Jay X. -ND. All rights reserved.
+//
+
+import Foundation
